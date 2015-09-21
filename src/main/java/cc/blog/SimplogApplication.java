afb40899e3b8ebe5b1cce5d366740d2eac2f8db2
t2vere <t2vere@gmail.com>
2015-09-21 15:28:15 +0900

    initial commit

---
+++ src/main/java/cc/blog/SimplogApplication.java
@@ -0,0 +1,12 @@
+package cc.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplogApplication.class, args);
+    }
+}
