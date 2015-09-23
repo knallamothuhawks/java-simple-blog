1ca0320063e1a171ace5cfe33f66c157d6088e95
t2vere <t2vere@gmail.com>
2015-09-23 18:09:27 +0900

    change unit test

--- src/main/java/cc/blog/service/PostService.java
+++ src/main/java/cc/blog/service/PostService.java
@@ -1,5 +1,7 @@
 package cc.blog.service;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +17,27 @@ public class PostService {
 	private PostRepository repository;
 	
 	public Long addPost(final Post post) {
+		final Date currentDate = Calendar.getInstance().getTime();
+		post.setCreatedDate(currentDate);
+		post.setLastModifiedDate(currentDate);
 		repository.save(post);
 		return post.getId();
+	}
+	
+	public void updatePost(final Post updatePost) {
+		if (updatePost == null || updatePost.getId() == null) {
+			throw new IllegalStateException("Invalid Post entity.");
+		}
+		
+		Post existPost = findPostById(updatePost.getId());
+		if (existPost == null) {
+			throw new IllegalStateException("Post entity not found, postId: " + updatePost.getId());
+		}
+		
+		updatePost.setCreatedDate(existPost.getCreatedDate());
+		updatePost.setLastModifiedDate(Calendar.getInstance().getTime());
+		
+		repository.update(updatePost);
 	}
 	
 	public Post findPostById(final Long postId) {
--- src/main/java/cc/blog/web/PostController.java
+++ src/main/java/cc/blog/web/PostController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,22 +19,27 @@ public class PostController {
 	@Autowired
 	private PostService service;
 	
-	@RequestMapping(value="/add/post", method=RequestMethod.POST)
-	public Long addPost(Post psot) {
-		return service.addPost(psot);
+	@RequestMapping(value="/post", method=RequestMethod.POST)
+	public Long addMember(Post post) {
+		return service.addPost(post);
 	}
 	
-	@RequestMapping(value="/view/post/id/{postId}", method=RequestMethod.GET)
+	@RequestMapping(value="/post", method=RequestMethod.PUT)
+	public void updatePost(@RequestBody Post post) {
+		service.updatePost(post);
+	}
+	
+	@RequestMapping(value="/post/{postId}", method=RequestMethod.GET)
 	public Post viewPost(@PathVariable(value="postId") Long postId) {
 		return service.findPostById(postId);
 	}
 	
-	@RequestMapping(value="/remove/post/id/{postId}")
+	@RequestMapping(value="/post/{postId}", method=RequestMethod.DELETE)
 	public void removePost(@PathVariable(value="postId") Long postId) {
 		service.removePostById(postId);
 	}
 	
-	@RequestMapping(value="/list/posts")
+	@RequestMapping(value="/posts", method=RequestMethod.GET)
 	public List<Post> listPosts() {
 		return service.findAllPosts();
 	}
